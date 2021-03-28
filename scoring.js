c15e543bbca634b919ff51190b03ca2e88f46931
Chadwick Smith <chadwicksmith@Chadwicks-MBP.hsd1.ct.comcast.net>
2021-03-28 18:38:31 -0400

    added a runningback function,

--- scoring.js
+++ scoring.js
@@ -1,29 +1,43 @@
- function qbStat(player) {
-     const qbPass = (player.stats.passing)
-     const yards = ((qbPass.yards) / 25 ) 
-     const touchdowns = ((qbPass.touchdowns) * 6 )
-     const interceptions = ((qbPass.interceptions) * -3)
-     const qbRush = (player.stats.rushing)
-     const rYards = ((qbRush.yards) / 10 )
-     const rTD = ((qbRush.touchdowns) * 6)
-     const rFum =((qbRush.fumbles) * -3)
-
-     
-
-
+ const qbStat = (player) => {
+     const Pass = player.stats.passing
+     const yards = Pass.yards / 25  
+     const touchdowns = Pass.touchdowns * 6 
+     const interceptions = Pass.interceptions * -3
+     const Rush = player.stats.rushing
+     const rYards = Rush.yards / 10 
+     const TD = Rush.touchdowns * 6
+     const Fum = Rush.fumbles * -3
+      return sum [yards + touchdowns + interceptions + rYards + TD + Fum]
  }
-   
+   const rbStat = (player) => {
+       const Rush = player.stats.rushing
+       const Yards = Rush.yards / 10
+       const TD = Rush.touchdowns * 6
+       const Fum = Rush.fumbles * -3
+       const receiving = player.stats.receiving
+       const rec = receiving.receptions * 1
+       const rYards = receiving.yards / 10
+       const rtouchdowns = receiving.touchdowns * 6
+       const rFum = receiving.fumbles * -3
+       const kReturns = player.stats.return.kickreturn
+       const pReturns = player.stats.return.puntreturn
+       const krYards = kReturns.yards / 10
+       const krTD = kReturns.touchdowns * 6
+       const krFum = kReturns.fumbles * -3
 
-
+   }
 
 function calculateScore(player) {
-    if (player.position === 'QB'){
+    if (player.position && 'QB' ) {
+        return qbStat
+}
+if  (player.position && 'RB'){
+    return rbStat
+}
 
-    }
 
 
 
 
-
-    
-module.exports = calculateScore()
+}
+ module.exports = calculateScore()
